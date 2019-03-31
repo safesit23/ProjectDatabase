b557dd88a5b3eaf0451545db39ec672285f893ef
Run <smashsarun@gmail.com>
2019-03-31 21:32:07 +0700

    [EDIT] manager role

--- projectDB.sql
+++ projectDB.sql
@@ -671,32 +671,17 @@ grant delete on TAXONOMY to role_vet;
 create role role_manager;
 grant role_staff to role_manager;
 
-grant SELECT on staff to role_manager;
-grant insert on staff to role_manager;
-grant UPDATE on staff to role_manager;
-grant delete on staff to role_manager;
+grant select, insert, update, delete on staff to role_manager;
 
-grant select on TAXONOMY to role_manager;
-grant insert on TAXONOMY to role_manager;
-grant UPDATE on TAXONOMY to role_manager;
-grant delete on TAXONOMY to role_manager;
+grant select, insert, update, delete on TAXONOMY to role_manager;
 
-grant select on MANAGEMENT to role_manager;
-grant insert on MANAGEMENT to role_manager;
-grant UPDATE on MANAGEMENT to role_manager;
-grant delete on MANAGEMENT to role_manager;
+grant select, insert, update, delete on MANAGEMENT to role_manager;
 
-grant insert on ANIMAL to role_manager;
-grant UPDATE on ANIMAL to role_manager;
-grant delete on ANIMAL to role_manager;
+grant insert, update, delete on ANIMAL to role_manager;
 
-grant insert on AREA to role_manager;
-grant UPDATE on AREA to role_manager;
-grant delete on AREA to role_manager;
+grant insert, update, delete on AREA to role_manager;
 
-grant insert on ZONE to role_manager;
-grant UPDATE on ZONE to role_manager;
-grant delete on ZONE to role_manager;
+grant insert, update, delete on ZONE to role_manager;
 
 
 -- ----------------------- CREATE USER -----------------------
